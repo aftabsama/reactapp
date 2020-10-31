5f87a9434c775a30a2fe824bbb531b8ed7881725
aftabsama <67306819+aftabsama@users.noreply.github.com>
2020-10-31 16:48:48 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  [
+    <h1>Hello CodeSandbox</h1>,
+    <h2>Start editing to see some magic happen!</h2>
+  ],
+  document.getElementById("root")
+);
